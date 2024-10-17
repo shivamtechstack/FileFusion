f13567e0f85a0c058cff805a02e83f5ef239fece
shivamtechstack <devshivamyadav1604@gmail.com>
2024-10-17 19:35:54 +0530

    Basic Zip Function add

---
+++ app/src/main/java/shivam/sycodes/filefusion/archievingAndEncryption/ZipArchieve.kt
@@ -0,0 +1,65 @@
+package shivam.sycodes.filefusion.archievingAndEncryption
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
+class ZipArchieve() {
+
+    fun zipFileorFolder(filesOrFolders: List<File>, outputZipPath: String) {
+        val zipFile = File(outputZipPath)
+        ZipOutputStream(BufferedOutputStream(FileOutputStream(zipFile))).use { zos ->
+            filesOrFolders.forEach { fileOrFolder ->
+                zipFileOrFolderRecursive(fileOrFolder, fileOrFolder.name, zos)
+            }
+        }
+    }
+
+    private fun zipFileOrFolderRecursive(fileOrFolder: File, parentDirPath: String, zos: ZipOutputStream) {
+        if (fileOrFolder.isDirectory) {
+
+            val folderName = "$parentDirPath/"
+            zos.putNextEntry(ZipEntry(folderName))
+            zos.closeEntry()
+
+            fileOrFolder.listFiles()?.forEach { file ->
+                zipFileOrFolderRecursive(file, "$parentDirPath/${file.name}", zos)
+            }
+        } else {
+            FileInputStream(fileOrFolder).use { fis ->
+                val zipEntry = ZipEntry(parentDirPath)
+                zos.putNextEntry(zipEntry)
+                fis.copyTo(zos, 1024)
+                zos.closeEntry()
+            }
+        }
+    }
+    fun unZipFile(zipFile: File, outputDirPath: String) {
+        val outputDir = File(outputDirPath)
+
+        if (!outputDir.exists()) {
+            outputDir.mkdirs()
+        }
+        ZipInputStream(BufferedInputStream(FileInputStream(zipFile))).use { zis ->
+            var zipEntry: ZipEntry?
+
+            while (zis.nextEntry.also { zipEntry = it } != null) {
+                val outputFile = File(outputDir, zipEntry!!.name)
+
+                if (zipEntry!!.isDirectory) {
+                    outputFile.mkdirs()
+                } else {
+                    FileOutputStream(outputFile).use { fos ->
+                        zis.copyTo(fos)
+                    }
+                }
+                zis.closeEntry()
+            }
+        }
+    }
+}
